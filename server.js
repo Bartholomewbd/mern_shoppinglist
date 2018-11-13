bec7927d71ed601df98e7808865524f7fc64cbaa
Brian Bartholomew <bartholomewbd@gmail.com>
2018-11-13 16:42:38 -0500

    Corrected the key and declarations for items

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -8,4 +8,12 @@ const app = express();
 app.use(bodyParser.json());
 
 //DB config 
-const db = require('./config/keys').mongoURI
+const db = require('./config/keys').mongoURI;
+
+//connect to Mongo
+
+mongoose.connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+  const port = process.env.portg
